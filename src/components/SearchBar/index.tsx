962030e044c55ad3785d4975454b4d288e98437b
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2024-10-03 12:07:45 +0300

    Create a search bar component

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TextInput, ViewStyle, Pressable } from "react-native";
+import Animated, {
+    useAnimatedStyle,
+    withTiming,
+} from "react-native-reanimated";
+
+import { EvilIcons, Entypo } from "@expo/vector-icons";
+import { spacing } from "../../theme/spacing";
+import useDebounce from "../../hooks/useDebounce";
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+interface SearchBarType {
+    style?: ViewStyle;
+    onSearch: (value: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarType> = ({ style, onSearch }) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const [value, setValue] = useState("");
+
+    const debouncedValue = useDebounce(value, 500);
+
+    useEffect(() => {
+        onSearch(debouncedValue);
+    }, [debouncedValue]);
+
+    const onFocus = () => {
+        setIsFocused(true);
+    };
+    const onBlur = () => {
+        setIsFocused(false);
+    };
+
+    const containerStyle = useAnimatedStyle(() => ({
+        paddingHorizontal: withTiming(isFocused ? "0%" : "7%"),
+    }));
+
+    const textInputStyle = useAnimatedStyle(() => ({
+        borderRadius: withTiming(isFocused ? 0 : 10),
+        height: withTiming(isFocused ? 50 : 40),
+        paddingHorizontal: withTiming(
+            isFocused ? spacing.large : spacing.small
+        ),
+        width: withTiming(isFocused ? "100%" : "90%"), // to leave space to the filtering button
+    }));
+
+    const clearIconStyle = useAnimatedStyle(
+        () => ({
+            transform: [
+                {
+                    translateX: withTiming(
+                        isFocused || value ? 0 : 20 + spacing.large
+                    ),
+                },
+            ],
+        }),
+        [value, isFocused]
+    );
+
+    return (
+        <Animated.View
+            style={[
+                {
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: spacing.small,
+                    width: "100%",
+                },
+                containerStyle,
+            ]}
+        >
+            <Animated.View style={[styles.container, style, textInputStyle]}>
+                <EvilIcons name="search" size={25} color="#8E8D92" />
+                <AnimatedTextInput
+                    returnKeyType="search"
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    autoCorrect={false}
+                    value={value}
+                    onChangeText={setValue}
+                    style={{
+                        flex: 1,
+                        marginLeft: spacing.tiny,
+                    }}
+                    placeholder={"Start typing to search a movie"}
+                    placeholderTextColor="#8E8D92"
+                    // selectionColor={colors.primary}
+                />
+                <AnimatedPressable
+                    onPress={() => setValue("")}
+                    style={clearIconStyle}
+                    hitSlop={10}
+                >
+                    <Entypo
+                        name="circle-with-cross"
+                        size={20}
+                        color="#8e8d9289"
+                    />
+                </AnimatedPressable>
+            </Animated.View>
+        </Animated.View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#f6f6f6",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+        width: "100%",
+    },
+});
+
+export default SearchBar;
